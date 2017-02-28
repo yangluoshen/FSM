7022f1e4e9b9b0fa63c8a2b5c90476e72eedf789
yangluo <649673800@qq.com>
2017-02-28 17:26:48 +0800

    server in epoll way

--- src/msg.h
+++ src/msg.h
@@ -7,9 +7,10 @@
 #define PTR_SIZE (sizeof(void*))
 
 #define MAX_CONTENT_LEN 1024
-#define SV_FIFO "sv_fifo"
-#define CL_FIFO_TPL "cl_fifo_%d"
-#define CL_FIFO_NAME_LEN (sizeof(CL_FIFO_TPL) + 20)
+#define SV_FIFO "/tmp/fsm_sv_fifo"
+#define SV_FIFO_TPL "/tmp/fsm_sv_fifo_%d"
+#define CL_FIFO_TPL "/tmp/fsm_cl_fifo_%d"
+#define FIFO_NAME_LEN (sizeof(CL_FIFO_TPL) + 20)
 
 typedef void* data_t;
 typedef size_t msg_type_t;
@@ -44,30 +45,30 @@ typedef struct{
 #define RESP_LEN (MSG_HEAD_LEN + sizeof(resp_t))
 
 /**** utilities ****/
-#define swap(__a, __b) \
-    do {\
-        (__a) ^= (__b);\
-        (__b) ^= (__a);\
-        (__a) ^= (__b);\
-    }while(0)
+#define swap(A, B) ({  __typeof__(A) __a = (A); __typeof__(B) __b = (B); __a ^= __b; __b ^= __a; __a ^= __b;})
 
+#define GEN_CL_NAME(buf, pid) \
+    do \
+        snprintf(buf, FIFO_NAME_LEN, CL_FIFO_TPL, pid);\
+    while(0)
+        
 /**** printer ****/
 #define PRINT_MSG(__msg) \
         printf("sender pid[%d],receiver pid[%d],sender module[%lu],receiver module[%lu],data_len[%lu]\n",\
-            (__msg)->s_pid,(__msg)->r_pid,(__msg)->s_mdl,(__msg)->r_mdl, (__msg)->data_len)
+            ((msg_t*)(__msg))->s_pid,((msg_t*)(__msg))->r_pid,((msg_t*)(__msg))->s_mdl,((msg_t*)(__msg))->r_mdl, ((msg_t*)(__msg))->data_len)
 
 #define print_req_msg(__msg, __req) \
     do {\
 		PRINT_MSG(__msg); \
         printf("msg_type[%lu], content[%s]\n",\
-            (__req)->msg_type, (__req)->what);\
+            ((req_t*)(__req))->msg_type, ((req_t*)(__req))->what);\
     }while(0)
 
 #define print_resp_msg(__msg, __resp) \
 	do{ \
 		PRINT_MSG(__msg);\
 		printf("msg_type[%lu], err_code[%lu]\n",\
-				(__resp)->msg_type, (__resp)->err_code);\
+				((resp_t*)(__resp))->msg_type, ((resp_t*)(__resp))->err_code);\
 	}while(0)
 
 /**** enums ****/
@@ -81,5 +82,10 @@ enum E_MODULE_TYPE{
 	UKU    /*unknown unit*/
 };
 
-#endif
 
+#define MAX_SV_EPOLL_NUM (2048)
+#define MAX_EVENTS (1024)
+#define TIMEOUT_SV_EPOLL (2 * 1000)
+#define MAX_DATA_LEN (4096)
+
+#endif
