b4fdec4d52542e8c7d6b5620aa33ff45a99930b7
yangluo <649673800@qq.com>
2017-02-28 17:27:35 +0800

    server in epoll way

--- src/client.c
+++ src/client.c
@@ -10,7 +10,7 @@
 
 #include "msg.h"
 
-static char client_fifo [CL_FIFO_NAME_LEN];
+static char client_fifo [FIFO_NAME_LEN];
 static char dfl_content[] = "hello server";
 
 void remove_cl_fifo()
@@ -18,15 +18,42 @@ void remove_cl_fifo()
     unlink(client_fifo);
 }
 
+int read_resp()
+{
+    int cl_fd = open(client_fifo, O_RDONLY);
+    assert(cl_fd != -1);
+
+    /* read msg head first */
+    msg_t msg;
+    if (read(cl_fd, &msg, MSG_HEAD_LEN) != MSG_HEAD_LEN){
+        perror("client read response failed");
+        return -1;
+    }
+    char* resp_buf = (char*) malloc(msg.data_len);
+    assert(resp_buf);
+    if (read(cl_fd, resp_buf, msg.data_len) != msg.data_len){
+        perror("client: Error reading resp");
+        return -1;
+    }
+    
+    req_t* preq = (req_t*) resp_buf;
+    print_req_msg(&msg, preq);
+
+    puts("client exit");
+    free(resp_buf);
+
+    return 0;
+}
+
 int main(int argc, char* argv[])
 {
-    int cl_fd, sv_fd;
+    int  sv_fd;
     char *content = dfl_content;
     
     if (argc > 1) content = argv[1];
 
     umask(0);
-    snprintf(client_fifo, CL_FIFO_NAME_LEN, CL_FIFO_TPL, getpid());
+    snprintf(client_fifo, FIFO_NAME_LEN, CL_FIFO_TPL, getpid());
 
     if (mkfifo(client_fifo, S_IRUSR|S_IWUSR|S_IWGRP) == -1 &&
         EEXIST != errno){
@@ -42,7 +69,7 @@ int main(int argc, char* argv[])
 
     msg_t* pmsg = (msg_t*)req_buf; 
     pmsg->s_pid = getpid();
-    pmsg->r_pid = -1;
+    pmsg->r_pid = pmsg->s_pid;
     pmsg->s_mdl = YAU;
     pmsg->r_mdl = DVU;
     pmsg->data_len = data_len;
@@ -65,24 +92,11 @@ int main(int argc, char* argv[])
         return -1;
     }
 
-    cl_fd = open(client_fifo, O_RDONLY);
-    assert(cl_fd != -1);
 
-
-    char* resp_buf = (char*) malloc(RESP_LEN);
-    assert(resp_buf);
-
-    if (read(cl_fd, resp_buf, RESP_LEN) != RESP_LEN){
-        perror("client read response failed");
+    if(-1 == read_resp()){
+        perror("read_response");
         return -1;
     }
-    pmsg = (msg_t*) resp_buf;
-    resp_t* presp = (resp_t*) pmsg->data;
-    print_resp_msg(pmsg, presp); puts("");
-
-    puts("client exit");
-    free(resp_buf);
 
     return 0;
-    
 }
